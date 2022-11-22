24a42d0065e6d4dce7a9de8973f29976328c7b12
Pierre Mengal <p@mengal.tech>
2022-11-22 14:37:49 +0100

    Removes all references to dead API PlayMate

--- libraries/dataGetters.js
+++ libraries/dataGetters.js
@@ -14,30 +14,6 @@ function getAOE4WorldData(profileID) {
     });
 }
 
-function getPlayMateData(profileID, gameID, teamID, playerID) {
-    return new Promise(function (resolve, reject) {
-        try {
-            request('https://api.playmate.gg/player/' + profileID, function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    try {
-                        let data = JSON.parse(body);
-                        data['request'] = {};
-                        data['request']['gameID']       = gameID;
-                        data['request']['teamID']       = teamID;
-                        data['request']['playerID']     = playerID;
-                        return resolve(data);
-                    } catch (e) {
-                        reject();
-                    }
-                }
-                reject();
-            })
-        } catch (err) {
-            reject();
-        }
-    });
-}
-
 function getAOE4PlayerLastGames(profileID, since) {
     return new Promise(function (resolve, reject) {
         try {
@@ -54,25 +30,6 @@ function getAOE4PlayerLastGames(profileID, since) {
                             for (var [teamID, team] of Object.entries(game.teams)) {
                                 for (var [playerID, player] of Object.entries(team)) {
                                     totalPlayers++;
-                                }
-                            }
-                        }
-
-                        for (var [gameID, game] of Object.entries(originalData.games)) {
-                            for (var [teamID, team] of Object.entries(game.teams)) {
-                                for (var [playerID, player] of Object.entries(team)) {
-                                    getPlayMateData(player.player.profile_id, gameID, teamID, playerID).then(function (playmateData) {
-                                        let recGameID = playmateData['request']['gameID'];
-                                        let recTeamID = playmateData['request']['teamID'];
-                                        let recPlayerID = playmateData['request']['playerID'];
-                                        newData['games'][recGameID]['teams'][recTeamID][recPlayerID]['playmate'] = playmateData;
-                                        extraDataPlayers++;
-                                        if (extraDataPlayers == totalPlayers) {
-                                            return resolve(newData);
-                                        }
-                                    }, function (err) {
-                                        return resolve(originalData);
-                                    });
                                 }
                             }
                         }
