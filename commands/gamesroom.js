bbc60eae4b62ad672b060d75c10c58af5f31e471
donPabloM <pmurzyniak@gmail.com>
2023-02-22 02:56:10 +0100

    addded bool param to unsetup gamesRoom and databaseMethod to remove it from database, and removal from node object guildSettings

--- commands/gamesroom.js
+++ commands/gamesroom.js
@@ -1,15 +1,21 @@
 const { Client, Intents, EmbedBuilder, Permissions, SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
-var {saveSetting} = require('../libraries/databaseMethods.js');
+var { saveSetting, removeSetting } = require('../libraries/databaseMethods.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('gamesroom')
 		.setDescription('Use this command to define the discord channel where the bot will print the game reports')
+                .addIntegerOption(option => option.setName('should_unsetup').setDescription("Pass 'true' if you want to unregister this channel as gamesroom").setRequired(false))
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 	async execute(interaction) {
         let guildID = interaction.guildId;
         let channelID = interaction.channelId;
-        saveSetting(guildID, 'gamesroom', channelID);
+                let shouldUnsetup = interaction.options.getBoolean('shouldUnsetup')
+                if (shouldUnsetup) {
+                        removeSetting(guildID, 'gamesroom', channelID);
+                } else {
+                saveSetting(guildID, 'gamesroom', channelID);
+                }
         await interaction.reply('This channel is now the games room.');
 	},
 };
